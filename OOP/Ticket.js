61e926687b65be18698e64f7dfa223aed43aae0a
yakovtfi <205momi@gmail.com>
2026-01-08 12:51:24 +0200

    Update Ticket.js

--- OOP/Ticket.js
+++ OOP/Ticket.js
@@ -2,7 +2,7 @@ class Ticket{
     constructor(Price,ownerName=null ){
         this.RandomTicketNumber = Math.floor(Math.random()*100000);
         this.Price = Price;
-        this.Ownername = ownerName;
+        this.ownerName = ownerName;
     }
 }
 
